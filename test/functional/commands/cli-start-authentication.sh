186fbc8f48b814a0d995bff4814b3f07a209570a
Javier Brea <javier.brea@gmail.com>
2018-08-25 10:48:35 +0200

    WIP - Add jwt authentication methods

---
+++ test/functional/commands/cli-start-authentication.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+npm run domapic-controller start controller -- --hostName=${controller_host_name} --path=${domapic_path} --db=${db_uri} --save --logLevel=trace --authDisabled
+npm run domapic-controller logs controller -- --path=${domapic_path}
--- test/functional/commands/cli-start.sh
+++ test/functional/commands/cli-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-npm run domapic-controller start controller -- --hostName=${controller_host_name} --path=${domapic_path} --db=${db_uri} --save --logLevel=trace
+npm run domapic-controller start controller -- --hostName=${controller_host_name} --path=${domapic_path} --db=${db_uri} --save
 npm run domapic-controller logs controller -- --path=${domapic_path}
