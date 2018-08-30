070ce94e8c67e5a4dbfa10e3f2c3663ee3fb3e08
Javier Brea <javier.brea@gmail.com>
2018-08-30 19:09:52 +0200

    Add users api functional tests

--- test/functional/specs/users-api.specs.js
+++ test/functional/specs/users-api.specs.js
@@ -20,22 +20,65 @@ test.describe('users api', function () {
     })
   }
 
+  const ensureUserAndDoLogin = function (user) {
+    return utils.doLogin()
+      .then((auth) => {
+        authenticator = auth
+        return postUser(user).finally(() => utils.doLogin({
+          name: user.name,
+          email: user.email,
+          password: user.password
+        }).then(auth => {
+          authenticator = auth
+          return Promise.resolve()
+        }))
+      })
+  }
+
   const newUser = {
-    name: 'foo name',
-    role: 'admin',
-    email: 'foo@foo.com',
+    name: 'foo service',
+    role: 'service',
+    email: 'foo2@foo.com',
     password: 'foo'
   }
 
-  test.describe('when user is admin', () => {
-    test.before(() => {
-      return utils.doLogin()
-        .then(auth => {
-          authenticator = auth
-          return Promise.resolve()
-        })
-    })
+  const operatorUser = {
+    name: 'foo operator',
+    role: 'operator',
+    email: 'operator@foo.com',
+    password: 'foo'
+  }
 
+  const serviceUser = {
+    name: 'foo service user',
+    role: 'service',
+    email: 'service@foo.com',
+    password: 'foo'
+  }
+
+  const pluginUser = {
+    name: 'foo plugin',
+    role: 'plugin',
+    email: 'plugin@foo.com',
+    password: 'foo'
+  }
+
+  const serviceRegistererUser = {
+    name: 'foo service registerer',
+    role: 'service-registerer',
+    email: 'service-registerer@foo.com',
+    password: 'foo'
+  }
+
+  test.before(() => {
+    return utils.doLogin()
+      .then(auth => {
+        authenticator = auth
+        return Promise.resolve()
+      })
+  })
+
+  test.describe('when user is admin', () => {
     test.describe('add user', () => {
       test.it('should return a bad data error if no name is provided', () => {
         return postUser({
@@ -87,7 +130,7 @@ test.describe('users api', function () {
           role: 'admidsn'
         }).then((response) => {
           return Promise.all([
-            test.expect(response.body.message).to.contain('is not one of enum values: admin,service,plugin'),
+            test.expect(response.body.message).to.contain('is not one of enum values: admin,operator,service,service-registerer,plugin'),
             test.expect(response.statusCode).to.equal(422)
           ])
         })
@@ -109,10 +152,10 @@ test.describe('users api', function () {
 
       test.it('should add user to database if all provided data pass validation', () => {
         return postUser({
-          name: newUser.name,
-          role: newUser.role,
-          email: newUser.email,
-          password: newUser.password
+          name: operatorUser.name,
+          role: operatorUser.role,
+          email: operatorUser.email,
+          password: operatorUser.password
         }).then((addResponse) => {
           return getUsers()
             .then((getResponse) => {
@@ -120,9 +163,9 @@ test.describe('users api', function () {
               const user = getResponse.body[2]
               return Promise.all([
                 test.expect(user._id).to.equal(userId),
-                test.expect(user.name).to.equal(newUser.name),
-                test.expect(user.role).to.equal(newUser.role),
-                test.expect(user.email).to.equal(newUser.email),
+                test.expect(user.name).to.equal(operatorUser.name),
+                test.expect(user.role).to.equal(operatorUser.role),
+                test.expect(user.email).to.equal(operatorUser.email),
                 test.expect(user.createdAt).to.not.be.undefined(),
                 test.expect(user.updatedAt).to.not.be.undefined()
               ])
@@ -134,7 +177,7 @@ test.describe('users api', function () {
         return postUser({
           name: 'foo name 2',
           role: 'admin',
-          email: 'foo@foo.com',
+          email: operatorUser.email,
           password: 'foo'
         }).then((response) => {
           return Promise.all([
@@ -146,7 +189,7 @@ test.describe('users api', function () {
 
       test.it('should return a bad data error if an already existant name is provided', () => {
         return postUser({
-          name: 'foo name',
+          name: operatorUser.name,
           role: 'admin',
           email: 'foo2@foo.com',
           password: 'foo'
@@ -161,28 +204,97 @@ test.describe('users api', function () {
 
     test.describe('get users', () => {
       test.it('should return all existant users', () => {
-        const newUser2 = {
-          name: 'foo service',
-          role: 'service',
-          email: 'foo2@foo.com',
-          password: 'foo'
-        }
-        return postUser(newUser2).then(() => {
+        return postUser(newUser).then(() => {
           return getUsers()
             .then((getResponse) => {
               const user1 = getResponse.body[2]
               const user2 = getResponse.body[3]
               return Promise.all([
-                test.expect(user1.name).to.equal(newUser.name),
-                test.expect(user1.role).to.equal(newUser.role),
-                test.expect(user1.email).to.equal(newUser.email),
+                test.expect(user1.name).to.equal(operatorUser.name),
+                test.expect(user1.role).to.equal(operatorUser.role),
+                test.expect(user1.email).to.equal(operatorUser.email),
                 test.expect(user1.createdAt).to.not.be.undefined(),
-                test.expect(user2.name).to.equal(newUser2.name),
-                test.expect(user2.role).to.equal(newUser2.role),
-                test.expect(user2.email).to.equal(newUser2.email),
+                test.expect(user2.name).to.equal(newUser.name),
+                test.expect(user2.role).to.equal(newUser.role),
+                test.expect(user2.email).to.equal(newUser.email),
                 test.expect(user1.updatedAt).to.not.be.undefined()
               ])
             })
+        })
+      })
+    })
+  })
+
+  const testRole = function (user) {
+    test.describe(`when user has role "${user.role}"`, () => {
+      test.before(() => {
+        return ensureUserAndDoLogin(user)
+      })
+
+      test.describe('add user', () => {
+        test.it('should return a forbidden error', () => {
+          return postUser(newUser).then(response => {
+            return Promise.all([
+              test.expect(response.body.message).to.contain('Not authorized'),
+              test.expect(response.statusCode).to.equal(403)
+            ])
+          })
+        })
+      })
+
+      test.describe('get users', () => {
+        test.it('should return a forbidden error', () => {
+          return getUsers().then(response => {
+            return Promise.all([
+              test.expect(response.body.message).to.contain('Not authorized'),
+              test.expect(response.statusCode).to.equal(403)
+            ])
+          })
+        })
+      })
+    })
+  }
+
+  testRole(operatorUser)
+  testRole(serviceUser)
+  testRole(pluginUser)
+
+  test.describe(`when user has role "service-registerer"`, () => {
+    test.before(() => {
+      return ensureUserAndDoLogin(serviceRegistererUser)
+    })
+
+    test.describe('add user', () => {
+      test.it('should return 201 when adding a new user with role "service"', () => {
+        return postUser({
+          name: 'foo new service',
+          role: 'service',
+          email: 'fooNewService@foo.com',
+          password: 'foo'
+        }).then(response => {
+          return test.expect(response.statusCode).to.equal(201)
+        })
+      })
+
+      test.it('should return a forbidden error when adding a new user with role different to "service"', () => {
+        return postUser({
+          name: 'foo new admin',
+          role: 'admin',
+          email: 'fooNewAdmin@foo.com',
+          password: 'foo'
+        }).then(response => {
+          return test.expect(response.statusCode).to.equal(403)
+        })
+      })
+    })
+
+    test.describe('get users', () => {
+      test.it('should return a forbidden error', () => {
+        return getUsers().then(response => {
+          return Promise.all([
+            test.expect(response.body.message).to.contain('Not authorized'),
+            test.expect(response.statusCode).to.equal(403)
+          ])
         })
       })
     })
