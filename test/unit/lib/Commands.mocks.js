8bdf563902b03dd992cac3881921dac02c32afd2
Javier Brea <javier.brea@gmail.com>
2018-09-09 13:00:48 +0200

    Add apiKey for registerer user on service initialization if it does not already exists

--- test/unit/lib/Commands.mocks.js
+++ test/unit/lib/Commands.mocks.js
@@ -2,18 +2,22 @@ const test = require('narval')
 
 const UserCommands = require('./commands/User.mocks')
 const SecurityTokenCommands = require('./commands/SecurityToken.mocks')
+const ComposedCommands = require('./commands/Composed.mocks')
 
 const Mock = function () {
   const sandbox = test.sinon.createSandbox()
   const userCommandsMocks = new UserCommands()
   const securityTokenMocks = new SecurityTokenCommands()
+  const composedMocks = new ComposedCommands()
 
   const stubs = {
     user: userCommandsMocks.stubs.commands,
-    securityToken: securityTokenMocks.stubs.commands
+    securityToken: securityTokenMocks.stubs.commands,
+    composed: composedMocks.stubs.commands
   }
 
   const restore = function () {
+    composedMocks.restore()
     userCommandsMocks.restore()
     securityTokenMocks.restore()
     sandbox.restore()
